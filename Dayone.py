feedf61671a84cba3d48691a73c33a9ba45961bb
Rachel Iroru <racheliroru2012@gmail.com>
2018-11-19 16:53:24 +0300

    add these dayh one file

---
+++ Dayone.py
@@ -0,0 +1,9 @@
+input = input("Enter year of birth")
+age = int(input)
+if age < 18:
+    print ("Minor")
+else:
+    if age > 18 and age < 37:
+        print("Youth")
+    else:
+        print("Elder")
