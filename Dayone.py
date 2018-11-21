a1388631cffe5f98480330d435d5898956060d69
Rachel Iroru <44372458+RachelIroru@users.noreply.github.com>
2018-11-22 02:00:58 +0300

    Update Dayone.py

--- Dayone.py
+++ Dayone.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 input = input("Enter year of birth")
 age = int(input)
 if age < 18:
@@ -8,7 +8,7 @@ input = input("Enter year of birth") //prompts the user to enter their date of b
 age = int(input) //user should input variable type interger
 if age < 18: //condition 
     print ("Minor")
->>>>>>> 3d322d5d836f621921d94bf3cf89f850bc821eaf
+
 else:
     if age > 18 and age < 37:
         print("Youth")
