769cdfa1ce9524cd65c654f983e5c579065e290a
Rachel Iroru <44372458+RachelIroru@users.noreply.github.com>
2018-11-22 03:17:55 +0300

    Delete Dayone-2.py

--- Dayone-2.py
+++
@@ -1,4 +0,0 @@
-d = dict() #creates an empty dictionary
-for X in range (2,15): #tells the system to output values in that range
-    d [X]=X**2 #for the values above multiply each by 2 to get the squares
-    print (d) #output the value of d
