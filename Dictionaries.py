94f1c9fd7ef0f18659ee7141403552ade4226237
Rachel Iroru <racheliroru2012@gmail.com>
2018-11-26 10:30:46 +0300

    Dealing with dictionaries

--- Dictionaries.py
+++ Dictionaries.py
@@ -1,4 +1,4 @@
 d= dict() #creating an empty dictionary
 for X in range (2,15): #output the values in that range
     d [X]=X**2 # square the values
-    print (d) 
+print (d) 
--- Output DOB.py
+++ Output DOB.py
@@ -1,10 +1,12 @@
 
 input = input("Enter year of birth") #prompts the user to enter their date of birth
 age = int(input) #user should input variable type interger
+input= str((2018- year of birth)
 if age < 18: #condition 
     print ("Minor")
 else:
-    if age > 18 and age < 37:
+    if age > 18 & age < 37:
         print("Youth")
     else:
         print("Elder")
+ 
